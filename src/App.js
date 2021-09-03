a7920e4c4999fd2d29f0c2aa6ac8fe625c522c43
Felipe Yuji <felipedias0810@gmail.com>
2021-09-03 18:16:26 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useReducer} from 'react'
+import { act } from 'react-dom/test-utils'
+import useGet from './useGet'
+
+const url = 'https://mymoney-87a8c-default-rtdb.firebaseio.com/movimentacoes/2021-09.json'
+const url2 = 'http://httpbin.org/ip'
 
 function App() {
+  const data = useGet(url)
+  const data2 = useGet(url2)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MyMoney</h1>
+      {JSON.stringify(data)}
+      {data.loading && <p>Loading...</p>}
+      <pre>{JSON.stringify(data2)}</pre>
     </div>
   );
-}
-
+  }
 export default App;
